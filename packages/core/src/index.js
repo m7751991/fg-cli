2c07aa450b82032460e421fa3c555887abbc052e
zixi <zixi@jingling.com>
2022-10-09 09:51:13 +0800

    feat: 新增命令

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -3,15 +3,32 @@ const fse = require("fs-extra");
 const { program } = require("commander");
 const log = require("npmlog");
 const { core } = require("@m7751991/init");
-
-program.name("fgcli").version("1.0.0");
+const pkg = require("../../../package.json");
+const spawn = require("child_process").spawn;
+function checkNodeVersion() {
+  // process.version
+  log.info("node version: " + process.version);
+}
+checkNodeVersion();
+program.name("fgcli").version(pkg.version);
 program.command("init [name]").action(async (obj, options) => {
   const res = await fse.exists(process.cwd());
-  console.log(obj);
+  log.info(obj);
   if (res) {
     core(obj);
   } else {
     log.error("当前目录不存在");
   }
+});
+program.command("dev").action(async (obj, options) => {});
+program.command("build").action(async (obj, options) => {});
+program.command("merge").action(async (obj, options) => {
+  const child = spawn("git", ["rev-parse", "--abbrev-ref", "HEAD"]);
+  child.stdout.on("data", (data) => {
+    console.log(data.toString());
+  });
+  child.stderr.on("data", (data) => {
+    console.log(data.toString());
+  });
 });
 program.parse(process.argv);
